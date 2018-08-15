6fed5d13cae569af6569fbf36694ff6f96ee4f30
TaslimOseni <tasleemoseni@gmail.com>
2018-08-15 03:46:19 +0100

    Created Afemai folder.. initialized!

---
+++ nglocaltimes/src/main/java/com/crevation/nglocaltime/afemai/Afemai.java
@@ -0,0 +1,35 @@
+package com.crevation.nglocaltime.afemai;
+
+import com.crevation.nglocaltime.Time;
+import java.util.Calendar;
+import java.util.Date;
+
+
+/**
+ * Created by Taslim Oseni on 14/08/18.
+ */
+
+
+
+public class Afemai implements Time {
+
+    @Override
+    public String getTime(Date time) {
+        return null;
+    }
+
+    @Override
+    public String getTime(Calendar time) {
+        return null;
+    }
+
+    @Override
+    public String getTime(String time) throws NumberFormatException, ArrayIndexOutOfBoundsException {
+        return null;
+    }
+
+    @Override
+    public String getCurrentTime() {
+        return null;
+    }
+}
---
+++ nglocaltimes/src/main/java/com/crevation/nglocaltime/afemai/Constants.java
@@ -0,0 +1,50 @@
+package com.crevation.nglocaltime.afemai;
+
+/**
+ * Created by Taslim Oseni on 14/08/18.
+ */
+
+public class Constants {
+    static final String number1 = "Okpa";
+    static final String number2 = "Evha";
+    static final String number3 = "Eiya";
+    static final String number4 = "Edie";
+    static final String number5 = "Eise";
+    static final String number6 = "Eisa";
+    static final String number7 = "Iselua";
+    static final String number8 = "Ilei";
+    static final String number9 = "Ichile";
+    static final String number10 = "Igbe";
+
+
+    static final String number11 = "";
+    static final String number12 = "";
+    static final String number13 = "";
+    static final String number14 = "";
+
+    static final String number15 = "";
+
+
+    static final String number16 = "";
+    static final String number17 = "";
+    static final String number18 = "";
+    static final String number19 = "";
+    static final String number20 = "";
+    static final String number21 = "";
+    static final String number22 = "";
+    static final String number23 = "";
+    static final String number24 = "";
+    static final String number25 = "";
+    static final String number26 = "";
+    static final String number27 = "";
+    static final String number28 = "";
+    static final String number29 = "";
+    static final String number30 = "";
+
+
+    static final String AFTER_POINTER = " onode";
+    static final String BEFORE_POINTER = " oreletsi";
+
+    static final String MINUTE = " egege";
+    static final String OCLOCK = "Agogo";
+}
